a146c30f23557bef2f0a766878f845b66d7062b5
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2021-11-18 07:30:52 +0100

    responsiveness adjust

--- src/app/authentication/components/reset-password/reset-password.component.ts
+++ src/app/authentication/components/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BaseComponent } from '@core/base/base/base.component';
 
 @Component({
@@ -7,13 +8,25 @@ import { BaseComponent } from '@core/base/base/base.component';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
+  public resetPasswordForm!: FormGroup;
   public isLoggingIn: boolean = false;
   public showPassword: boolean = false;
   public showConfirmPassword: boolean = false;
 
-  constructor(private _base: BaseComponent) { }
+  constructor(
+    private _base: BaseComponent,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initResetPasswordForm();
+  }
+
+  public initResetPasswordForm() {
+    this.resetPasswordForm = this.fb.group({
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    })
   }
 
   public submit(): void {

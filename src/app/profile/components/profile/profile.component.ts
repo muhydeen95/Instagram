e9a9b4128785f628f1d78b4a47274c216f17a0ee
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2021-11-20 15:49:33 +0100

    profile page converted

--- src/app/profile/components/profile/profile.component.ts
+++ src/app/profile/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ChangePasswordDialogComponent } from 'app/profile/dialogs/change-password-dialog/change-password-dialog.component';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  public isLoading: boolean = false;
+  public updateProfileForm!: FormGroup;
 
-  constructor() { }
+  constructor(public dialog: MatDialog, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.initUpdateProfileForm();
+  }
+
+  initUpdateProfileForm() {
+    this.updateProfileForm = this.fb.group({
+        firstName: [''],
+        middleName: [''],
+        lastName: [''],
+        phoneNumber1: [''],
+        phoneNumber2: [''],
+        email: [''],
+        altEmail: [''],
+        organizationName: ['']
+    })
+  }
+
+  public openModal():void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.height = "480px";
+    dialogConfig.width = "700px";
+    dialogConfig.data = {
+      email: "firstname.lastname@domain.com",
+      counter: "00:05",
+    }
+    const dialogRef = this.dialog.open(ChangePasswordDialogComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  submit() {
+
   }
 
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ChangePasswordDialogComponent } from './dialogs/change-password-dialog/change-password-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule, SharedModule],
+  declarations: [ProfileComponent, ChangePasswordDialogComponent],
+  imports: [CommonModule, ProfileRoutingModule, MatDialogModule, ReactiveFormsModule, SharedModule],
 })
 export class ProfileModule {}

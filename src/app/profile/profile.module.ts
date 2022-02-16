f9019fb43ffce833022ddf8f289b676326371739
muhydeen95 <alabimuhydeen@gmail.com>
2022-02-16 19:58:13 +0100

    enable and disable profile form on edit

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -5,10 +5,10 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ChangePasswordDialogComponent } from './dialogs/change-password-dialog/change-password-dialog.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProfileComponent, ChangePasswordDialogComponent],
-  imports: [CommonModule, ProfileRoutingModule, MatDialogModule, ReactiveFormsModule, SharedModule],
+  imports: [CommonModule, ProfileRoutingModule, MatDialogModule, ReactiveFormsModule, SharedModule, FormsModule],
 })
 export class ProfileModule {}

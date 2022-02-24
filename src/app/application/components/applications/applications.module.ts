4cacb484ba9c10d4af12eec69060a54c35c2b633
Treasurechic <babychic140@gmail.com>
2022-02-24 15:37:01 +0100

    :zap: Added pagination to the lc application list

--- src/app/application/components/applications/applications.module.ts
+++ src/app/application/components/applications/applications.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ApplicationsRoutingModule } from './applications-routing.module';
 import { ApplicationsComponent } from './applications.component';
 import { SharedModule } from '@shared/shared.module';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [ApplicationsComponent],
-  imports: [CommonModule, ApplicationsRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    ApplicationsRoutingModule,
+    SharedModule,
+    MatPaginatorModule,
+  ],
 })
 export class ApplicationsModule {}

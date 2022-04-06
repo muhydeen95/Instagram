83c302bb50a551e499c4268848be0b1fb9f95b57
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2022-04-06 22:01:58 +0100

    routeguard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouteGuard } from '@core/guards/route.guard';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { OfflineComponent } from './offline/offline.component';
@@ -15,7 +16,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    // canActivate: [RouteGuard],
+    canActivate: [RouteGuard],
     children: [
       {
         path: '',
